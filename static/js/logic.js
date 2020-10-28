aea76c2fce373bd5c74339791d5fe671ec794471
Amanda <amandaenstad@gmail.com>
2020-10-27 20:31:28 -0500

    updated logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,37 +1,47 @@
-// Significant Earthquakes past 30 days - GEOJSON URL
-// url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
-
-
-
-// From Activity 17.3:
+// Same process as Activity 17.3:
 function createMap(earthquakes) {
 
   // Create the tile layer that will be the background of our map
-  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "satellite-streets-v11",
+    accessToken: API_KEY
+  });
+
+  // Create the Greyscale Layer
+  var greyscaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "light-v10",
     accessToken: API_KEY
   });
 
-  // Create a baseMaps object to hold the lightmap layer
+  // Create the Outdoors Layer
+  var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "outdoors-v11",
+    accessToken: API_KEY
+  });
+
+  // Create a baseMaps object to hold the satellite layer
   var baseMaps = {
-    "Light Map": lightmap
+    "Satellite": satelliteMap,
+    "Greyscale": greyscaleMap,
+    "Outdoors": outdoorsMap
   };
 
   // Create an overlayMaps object to hold the Earthquakes
   var overlayMaps = {
-    "Earthquakes": earthquakes
+    "Earthquakes": earthquakes,
   };
-
-
-
 
   // Create the map object with options
   var map = L.map("map-id", {
     center: [40.73, -74.0059],
     zoom: 3,
-    layers: [lightmap, earthquakes]
+    layers: [satelliteMap, earthquakes]
   });
 
   // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
@@ -40,85 +50,87 @@ function createMap(earthquakes) {
   }).addTo(map);
 }
 
-
+// Create the map markers
 function createMarkers(response) {
 
-    // Pull the "features" property off of response.data
-    var locations = response.features;
+  // Pull the "features" property off of response.data
+  var locations = response.features;
 
-    // Initialize an array to hold bike markers
-    var earthquakeMarkers = [];
+  // Initialize an array to hold bike markers
+  var earthquakeMarkers = [];
 
-    // Loop through the earthquakes array
-    for (var index = 0; index < locations.length; index++) {
-      
-        var earthquake = locations[index];
-        lon = earthquake.geometry.coordinates[0];
-        lat = earthquake.geometry.coordinates[1];
-        depth = earthquake.geometry.coordinates[2];
-        magnitude = earthquake.properties.mag;
-        detailLink = earthquake.properties.detail;
+  // Loop through the earthquakes array
+  for (var index = 0; index < locations.length; index++) {
 
-        // Logic to decide what colors the bubbles are
-        if (depth > 8) {
-          color = "red";
-        }
-        else if (depth > 5) {
-          color = "orange";
-        }
-        else if (depth > 3) {
-          color = "yellow";
-        }
-        else {
-          color = "grey";
-        }
+    var earthquake = locations[index];
+    lon = earthquake.geometry.coordinates[0];
+    lat = earthquake.geometry.coordinates[1];
+    depth = earthquake.geometry.coordinates[2];
+    magnitude = earthquake.properties.mag;
+    detailLink = earthquake.properties.detail;
 
-        // For each earthquake location, create a marker and bind a popup with the earthquakes's name
-        var earthquakeMarker = L.circle(([lat, lon]), {
-          radius: (magnitude * 20000),
-          color: color,
-          fillColor: color
+    // Logic to decide what colors the bubbles are
+    if (depth > 8) {
+      color = "red";
+    }
+    else if (depth > 5) {
+      color = "orange";
+    }
+    else if (depth > 3) {
+      color = "yellow";
+    }
+    else {
+      color = "grey";
+    }
 
-        }).bindPopup("<h3> Location: " + earthquake.properties.place + "</h3><h3>Magnitude: " + magnitude + "</h3>" + "</h3><h3>Depth: " + depth + "</h3>" + "</h3><h3><a href='" + detailLink + "' target='_blank'>See Details</a></h3>");
+    // For each earthquake location, create a marker and bind a popup with the earthquakes's name
+    var earthquakeMarker = L.circle(([lat, lon]), {
+      radius: (magnitude * 20000),
+      color: color,
+      fillColor: color
 
-        // Add the marker to the bikeMarkers array
-        earthquakeMarkers.push(earthquakeMarker);
-      }
+    }).bindPopup("<h3> Location: " + earthquake.properties.place + "</h3><h3>Magnitude: " + magnitude + "</h3>" + "</h3><h3>Depth: " + depth + "</h3>" + "</h3><h3><a href='" + detailLink + "' target='_blank'>See Details</a></h3>");
 
+    // Add the marker to the bikeMarkers array
+    earthquakeMarkers.push(earthquakeMarker);
+  }
 
-
-  // Create a layer group made from the bike markers array, pass it into the createMap function
+  // Create a layer group made from the earthquake markers array, pass it into the createMap function
   createMap(L.layerGroup(earthquakeMarkers));
 }
+
+
+//Add a legend 
+//chloropleth activity
+
 
 // Perform the API call to retrieve earthquake info
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
 
+
+//////////// now entering the work in progress section /////////////////
+
+// Techtonic plates
+d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(createTechtonicPlates)
+
+function createTechtonicPlates(response) {
+  var coordinates = response.features[0].geometry.coordinates;
+  var geometry = response.features[0].geometry;
+
+  var techtonicPlates = [geometry];
+
+  L.geoJSON(techtonicPlates, {
+  }).addTo(map);
+
+
+
 // GEOJSON
 
+// function addTechtonicPlates(techtonicPlates) {
+// d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json", function(tdata) {
+//   L.geoJSON(tdata, {
+//   }).addTo(techtonicPlates);
+//   techtonicPlates.addTo(map);
 
-
-
-
-// for (var index = 0; index < geojsonFeature.features.length; index++) {
-//   plateCoordinates.push(geojsonFeature.geometry.coordinates[index]);
+// });
 // }
-
-  
-// var myLines = [{
-//   "type": "LineString",
-//   "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-// }, {
-//   "type": "LineString",
-//   "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-// }];
-
-// var myStyle = {
-//   "color": "#ff7800",
-//   "weight": 5,
-//   "opacity": 0.65
-// };
-
-// L.geoJSON(myLines, {
-//   style: myStyle
-// }).addTo(map);
